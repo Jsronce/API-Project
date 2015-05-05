7fc8e5288196bce07c98ed42fd339d8e8c69465f
Jsronce <js82@live.missouristate.edu>
2015-05-05 16:27:02 -0500

    Allow advancing of data in crontroller

--- Imgur-controller.js
+++ Imgur-controller.js
@@ -2,34 +2,44 @@
 
 var AppControllers = angular.module('AppControllers', [])
 
-AppControllers.controller('ImgurController', ['$scope', '$http', function ($scope, $http) {
-	
+AppControllers.controller('ImgurController', ['$scope', '$http', "$location", function ($scope, $http, $location) {
 	$scope.search = function(tag){
-		var searchTerm = tag
-		var request = {
-			method: 'GET',
-			url: 'https://api.imgur.com/3/gallery/t/'+searchTerm,
-			headers: {
-				Authorization: 'Client-ID c2d46d060a1041c'
-		}}
-		$http(request).success(function(data) {
-		console.log(data.data.items[0].link)
-		var videos = []
-		var temp
-		for(var i = 0; i <data.data.items.length; i++){
-			temp = data.data.items[i]
-			if(!(temp.mp4 === undefined)){
-				videos.push(temp)
-			}
-			if (videos.length > 4){
-			break}
+		if(tag != ''){
+			$location.path(tag)
 		}
-		console.log(videos)
-		$scope.response = videos;
-		console.log($scope.response)
-		
-  });
-	}
+		if($location.path() != ""){
+			var searchTerm = $location.path()
+			var request = {
+				method: 'GET',
+				url: 'https://api.imgur.com/3/gallery/t'+searchTerm,
+				headers: {
+					Authorization: 'Client-ID c2d46d060a1041c'
+			}}
+			$http(request).success(function(data) {$scope.data = data});
+			data = $scope.data
+			var videos = [];
+			var temp;
+			var count = 1;
+			for(var i = 0;1; i++){
+				if(i>=data.data.items.length){
+					request.url = 'https://api.imgur.com/3/gallery/t'+searchTerm + '/' +(count+1)
+					$http(request).success(function(data) {scope.data = data}).error(function(data, status){$scope.status = status});
+					if(status == "404"){break}
+					i = 0;
+				}
+				temp = data.data.items[i]
+				if(!(temp.mp4 === undefined)){
+					videos.push(temp)
+				}
+				if (videos.length > 4){
+				break}
+			}
+			console.log(videos)
+			$scope.response = videos;
+			console.log($scope.response)
+	}}
+
+	$scope.search('')
 }]);
 
 
